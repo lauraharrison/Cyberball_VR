0854b648c9f083b044f7187951ad71583b5a69b2
Daniel Batista <Daniel Batista>
2017-03-08 00:40:04 -0800

    adding animations and integration with the scene BedroomCharacters

--- Assets/Brain/scripts/BallTosser.cs
+++ Assets/Brain/scripts/BallTosser.cs
@@ -11,19 +11,31 @@ public class BallTosser : MonoBehaviour {
     public GameObject ballPrefab;
     public float tossSpeedx = 4f;
     float tossSpeedy = 5f;
-    public bool haveBall=true;
+    public bool haveBall=false;
 
     public GameObject myBall;
     public bool isplayer;
     public float includePlayerPercentage = 0.5f;
-
+	
+	public float throwTime = 0.8f;
+	public float smoothRot = 0.5f;
+ 	Animator myAnim;
+	Transform myTransform;
+	Vector3 newForth = new Vector3();
+	bool lookingTarget;
 
     // Use this for initialization
     void Start () {
-        if(!isplayer)
+		myTransform = transform;
+		
+        if(!isplayer){
+			myAnim = GetComponent<Animator>();
             myBall.SetActive(false);
-        else
+		}
+        else{
+			myAnim = GameObject.Find(gameObject.name+"/meshPlayer").GetComponent<Animator>();
             StartCoroutine(GetReady2Play());
+		}
     }
 	
 	// Update is called once per frame
@@ -34,27 +46,50 @@ public class BallTosser : MonoBehaviour {
             {
                 if (Input.GetKeyDown(KeyCode.L))
                 {
-                    ThrowBall(false);
+					myBall.SetActive(false);
+                    StartCoroutine(AnimateThrow(false));
                 }
                 if (Input.GetKeyDown(KeyCode.J))
                 {
-                    ThrowBall(true);
+					myBall.SetActive(false);
+                    StartCoroutine(AnimateThrow(true));
                 }
             }
-            else {
+            else {				
                 StartCoroutine(DecideAndThrow());
             }
         }
+		if(lookingTarget && !isplayer){
+			myTransform.forward = Vector3.Lerp (myTransform.forward, newForth, smoothRot*Time.deltaTime);
+		}
 	}
 
     IEnumerator DecideAndThrow() {
         haveBall = false;
+		
+		//Random wait before throwing the next ball
         yield return new WaitForSeconds(Random.Range(0.0f, 5.0f));
         if(Random.Range(0.0f, 1.0f) <= includePlayerPercentage)
-            ThrowBall(false);
+            StartCoroutine(AnimateThrow(false));
         else
-            ThrowBall(true);
+            StartCoroutine(AnimateThrow(true));
     }
+	
+	IEnumerator AnimateThrow(bool left){
+		if (left)
+        {
+            newForth = targetLeft.position - myTransform.position;
+		}
+		else{
+			newForth = targetRight.position - myTransform.position;
+		}
+		lookingTarget = true;
+		
+		myAnim.SetBool("throw",true);
+		yield return new WaitForSeconds(throwTime);
+		ThrowBall(left);
+		myAnim.SetBool("throw",false);
+	}
     void ThrowBall(bool left) {
         Vector3 origin;
         Transform target;
@@ -79,18 +114,21 @@ public class BallTosser : MonoBehaviour {
         tossSpeedy = (-1) * gravityAcel * (Vector3.Distance(target.position,origin)) / (2 * tossSpeedx);
         ball.GetComponent<Rigidbody>().velocity = ballTrans.forward * tossSpeedx + ballTrans.up * tossSpeedy;
         
+		float impactTime = Vector3.Distance(target.position,origin)/tossSpeedx;
+		target.GetComponent<BallTosser>().prepareToTakeBall(impactTime);
 		//Debug.DrawRay(ballTrans.position, target.position - ballTrans.position, new Color (1f, 1f, 0f));
 		//Time.timeScale = 0;
 		
 		haveBall = false;
         myBall.SetActive(false);
+		lookingTarget = false;
 
         //while there are no other players to throw the ball back...
         //StartCoroutine(RecuperateBall());        
     }
     IEnumerator RecuperateBall() {
         yield return new WaitForSeconds(0.8f);
-        haveBall = true;
+        haveBall = true;		
         myBall.SetActive(true);
     }
     IEnumerator GetReady2Play()
@@ -99,14 +137,23 @@ public class BallTosser : MonoBehaviour {
         haveBall = true;
         myBall.SetActive(true);        
     }
-
+	void prepareToTakeBall(float impactIn){
+		StartCoroutine(CatchBall(impactIn));
+	}
+	IEnumerator CatchBall(float impactIn) {
+		yield return new WaitForSeconds(impactIn/2);
+		myAnim.SetBool("catch",true);
+        yield return new WaitForSeconds(0.8f);
+		myAnim.SetBool("catch",false);
+		myBall.SetActive(true);
+        haveBall = true;
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "cyberball")
-        {
-            haveBall = true;
-            myBall.SetActive(true);
+        {            		
             Destroy(other.gameObject);
+			//prepareToTakeBall(CatchBall());
         }
         Debug.Log(gameObject.name + " was triggered by "+other.gameObject.name);
     }
